6beb1b1e452458fabeeeeba3a369282f91bfdbd5
Connor Hindley <conn.hindley@gmail.com>
2013-11-14 19:03:10 -0500

    test webhook

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -116,6 +116,7 @@
 		<!-- ./ container -->
 
 		<!-- Javascripts
+		<p>Hi Github</p>
 		================================================== -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
