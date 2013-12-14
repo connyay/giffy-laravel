223cbe297e00dfb23a050ef637488fa15e15517f
Connor Hindley <conn.hindley@gmail.com>
2013-12-13 20:43:22 -0500

    update mine view

--- app/views/gifs/mine.blade.php
+++ app/views/gifs/mine.blade.php
@@ -17,7 +17,9 @@ All Gifs
 
 		<div class="col-sm-6 col-xs-12 col-md-3 col-lg-3 giffy-thumb">
 			<div class="thumbnail well">
-				<img class="giffy-thumb" src="{{{URL::to($gif->thumb)}}}" data-thumb-src="{{{URL::to($gif->thumb)}}}" data-full-src="{{{$gif->url}}}" />
+				<a href="{{ URL::to('gifs', array('id'=>$gif->id)) }}">
+					<img class="img" src="{{{URL::to($gif->thumb)}}}" data-thumb-src="{{{URL::to($gif->thumb)}}}" data-full-src="{{{$gif->url}}}" />
+				</a>
 				<div class="caption">
 				
 					<p><a href="{{ URL::to('gifs/show', array('id'=>$gif->id)) }}" class="btn btn-primary btn-block">Open</a></p>
