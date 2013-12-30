0222820ba530ed00202d3957445c75eb56366c5f
Connor Hindley <conn.hindley@gmail.com>
2013-12-30 04:19:15 -0600

    oops... Still need the longer sleep

--- app/Giffy/Commands/GiffyGfyCatCommand.php
+++ app/Giffy/Commands/GiffyGfyCatCommand.php
@@ -39,33 +39,33 @@ class GiffyGfyCatCommand extends Command {
 	 */
 	public function fire() {
 		$count = 0;
-		$gifs = Gif::orderBy('id', 'DESC')->where( 'gfy_id', '=', NULL )->take( 10 )->get();
-        foreach ( $gifs as $gif ) {
-        	$count++;
-        	$transaction_id = str_pad( $count, 3, "0", STR_PAD_LEFT );
-            $url = $gif->url;
-            $response = (array) json_decode( file_get_contents( 'http://upload.gfycat.com/transcode/GiffyCo'.$transaction_id.'?fetchUrl='.$url ), true );
-            $gfyName = $response['gfyName'];
-            if ( !is_null( $gfyName ) && !empty( $gfyName ) ) {
-                $response = (array) json_decode( file_get_contents( 'http://gfycat.com/ajax/publish/'.$gfyName ), true );
-                $gifSize = $response['gifSize'];
-                if ( !is_null( $gifSize ) && !empty( $gifSize ) ) {
-                    $gif->gfy_id = $gfyName;
-                    $gif->save();
-                    $this->info('Made ' . $gfyName . ' for imgur link ' . $url);
-                    $this->info('Sleeping for 10');
-                    sleep(10);
-                    $this->info('Awake!');
-                }
-            }
-        }
-        if($this->runCount == 0) {
-        	$this->info('Made some GfyLinks...');
-        	$this->runCount++;
-        	$this->fire();
-    	} else {
-    		$this->info('Made a lot of GfyLinks...');
-    	}
+		$gifs = Gif::orderBy( 'id', 'DESC' )->where( 'gfy_id', '=', NULL )->take( 10 )->get();
+		foreach ( $gifs as $gif ) {
+			$transaction_id = str_pad( $count++, 3, "0", STR_PAD_LEFT );
+			$url = $gif->url;
+			$response = (array) json_decode( file_get_contents( 'http://upload.gfycat.com/transcode/GiffyCo'.$transaction_id.'?fetchUrl='.$url ), true );
+			if ( !array_key_exists( 'gfyName', $response ) ) {
+				$this->error( 'Something went wrong... skipping.' );
+				sleep( 60 );
+				continue;
+			}
+			$gfyName = $response['gfyName'];
+			if ( !is_null( $gfyName ) && !empty( $gfyName ) ) {
+				$gif->gfy_id = $gfyName;
+				$gif->save();
+				$this->info( 'Made ' . $gfyName . ' for imgur link ' . $url );
+				$this->info( 'Sleeping for 60' );
+				sleep( 60 );
+				$this->info( 'Awake!' );
+			}
+		}
+		if ( $this->runCount == 0 ) {
+			$this->info( 'Made some GfyLinks...' );
+			$this->runCount++;
+			$this->fire();
+		} else {
+			$this->info( 'Made a lot of GfyLinks...' );
+		}
 
 	}
 
