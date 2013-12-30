94662b6c7fad4274452d552fbcdd95205d3a371d
Connor Hindley <conn.hindley@gmail.com>
2013-12-30 03:16:16 -0600

    use artisan command for db:seed

--- app/database/seeds/GifsTableSeeder.php
+++ app/database/seeds/GifsTableSeeder.php
@@ -4,27 +4,7 @@ class GifsTableSeeder extends Seeder {
 
 	public function run()
 	{
-		DB::table('gifs')->truncate();
-		
-		$gifs = array();
-
-		$thumbs = File::files(public_path() . '/thumbs');
-        
-        foreach ($thumbs as $thumb) {
-        	$fileName = last(explode("/", $thumb));
-        	$url = "http://i.imgur.com/" . $fileName;
-        	$thumbPath = "/thumbs/" . $fileName;
-
-        	$gifs[] = array(
-					'url' => $url,
-					'thumb' => $thumbPath,
-					'created_at' => new DateTime,
-                	'updated_at' => new DateTime,
-				);
-        }
-
-
-		DB::table('gifs')->insert($gifs);
+		Artisan::call('giffy:feed');
 	}
 
 }
