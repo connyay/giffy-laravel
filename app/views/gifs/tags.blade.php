516cf677ecf4867761616d23aeee5f3b5d6ead5a
Connor Hindley <conn.hindley@gmail.com>
2014-01-02 13:19:51 -0600

    encode tag names

--- app/views/gifs/tags.blade.php
+++ app/views/gifs/tags.blade.php
@@ -1,8 +1,8 @@
 <ol class="giffy-tag-cloud">
     <li>Tags:</li>
     @foreach(Auth::user()->tags()->has('gifs')->rememberForever()->cacheTags( 'tags' )->get() as $tag)
-    <li><a href="{{ URL::to('gifs/tag', array('tag'=>$tag->name)) }}">
-        <span class="label {{ (Request::is('gifs/tag/'.$tag->name) ? 'label-primary' : 'label-info') }}">
+    <li><a href="{{ URL::to('gifs/tag', array('tag'=>urlencode($tag->name))) }}">
+        <span class="label {{ (Request::is('gifs/tag/'.urlencode($tag->name)) ? 'label-primary' : 'label-info') }}">
             {{{ $tag->name }}} ({{ $tag->gifs()->rememberForever()->cacheTags( 'tags' )->count() }})</span>
         </a></li>
         @endforeach
